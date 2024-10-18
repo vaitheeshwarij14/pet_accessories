0ea9f8705206f109de5f9d40534ba2629157c604
Vaitheshwari J <154435285+vaitheeshwarij14@users.noreply.github.com>
2024-10-18 21:54:08 +0530

    added and modified src

---
+++ admin/src/Components/Addproduct/Addproduct.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import './Addproduct.css';
+import upload from '../../assets/upload.png';
+
+const AddProduct = () => {
+    const [image, setImage] = useState(false);
+    const [productDetails, setProductDetails] = useState({
+        name: "",
+        image: "",
+        category: "",
+        pricePerKg: "" 
+    });    
+    
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const imageHandler = (e) => {
+        setImage(e.target.files[0]);
+    };
+
+    const changeHandler = (e) => {
+        setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
+    };
+    
+
+    const addProduct = async () => {
+       
+        console.log(productDetails);
+        let responseData;
+        let product = productDetails;
+        setError(null);
+        let formData = new FormData();
+        formData.append('product', image);
+        
+        await fetch('http://localhost:5000/upload', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+            },
+            body: formData,
+        }).then((resp) => resp.json()).then((data) => { responseData = data; });
+    
+        if (responseData.success) {
+            product.image = responseData.image_url;
+            console.log(product);
+            await fetch('http://localhost:5000/addproduct', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(product),
+            }).then((resp) => resp.json()).then((data) => {
+                data.success ? alert("Product added") : alert("Failed");
+            });
+    
+        }
+    };
+    return (
+        <div className='addproduct'>
+            {error && <p className='error'>{error}</p>}
+            <div className="addproduct-itemfield">
+                <p>Product title</p>
+                <input
+                    value={productDetails.name}
+                    onChange={changeHandler}
+                    type='text'
+                    name="name"
+                    placeholder='Type here'
+                />
+            </div>
+            <div className="addproduct-price">
+                <div className="addproduct-itemfield">
+                    <p>Price</p>
+                    <input
+                        value={productDetails.pricePerKg}
+                        onChange={changeHandler}
+                        type='text'
+                        name="pricePerKg"
+                        placeholder='Type here'
+                    />
+                </div>
+            </div>
+            <div className="addproduct-itemfield">
+                <p>Product Category</p>
+                <select
+                    value={productDetails.category}
+                    onChange={changeHandler}
+                    name="category"
+                    className='add-product-selector'
+                >
+                    <option>--select--</option>
+                    <option value="vegetables">Vegetables</option>
+                    <option value="fruits">Fruits</option>
+                    <option value="dryfruits">Dryfruits</option>
+                </select>
+            </div>
+            <div className="addproduct-itemfield">
+                <label htmlFor='file-input'>
+                    <img
+                        src={image?URL.createObjectURL(image):upload}
+                        className='addproduct-img'
+                        alt="Upload"
+                    />
+                </label>
+                <input
+                    onChange={imageHandler}
+                    type="file"
+                    name="image"
+                    id="file-input"
+                    hidden
+                />
+            </div>
+            <button
+                onClick={addProduct}
+                className='addproduct-btn'
+                disabled={loading}
+            >
+                {loading ? 'Adding...' : 'ADD'}
+            </button>
+        </div>
+    );
+};
+
+export default AddProduct;
---
+++ admin/src/Components/Listproduct/Listproduct.jsx
@@ -0,0 +1,191 @@
+import React, { useEffect, useState } from 'react';
+import './Listproduct.css';
+
+const Listproduct = () => {
+  const [allProducts, setAllProducts] = useState([]);
+  const [editingProductId, setEditingProductId] = useState(null); 
+  const [editProductDetails, setEditProductDetails] = useState({}); 
+  const [searchTerm, setSearchTerm] = useState(''); 
+
+  const fetchInfo = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/allproducts');
+      const data = await response.json();
+      setAllProducts(data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const removeProduct = async (id) => {
+    const confirmDeletion = window.confirm("Are you sure you want to delete the product?");
+    
+    if (confirmDeletion) {
+      try {
+        await fetch('http://localhost:5000/removeproduct', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id }), 
+        });
+        await fetchInfo(); 
+      } catch (error) {
+        console.error('Error removing product:', error);
+      }
+    } else {
+      console.log("Product deletion canceled.");
+    }
+  };
+  
+
+  const updateProduct = async (productId) => {
+    try {
+      const productDetails = editProductDetails[productId]; 
+      const updatedProductData = {
+        id: productId, 
+        name: productDetails.name,
+        pricePerKg: productDetails.pricePerKg,
+        category: productDetails.category || 'defaultCategory',
+      };
+      await fetch('http://localhost:5000/updateproduct', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedProductData), 
+      });
+      await fetchInfo();
+      setEditingProductId(null);
+    } catch (error) {
+      console.error('Error updating product:', error);
+    }
+  };
+
+  const handleEditChange = (e, productId) => {
+    const { name, value } = e.target;
+    setEditProductDetails((prevDetails) => ({
+      ...prevDetails,
+      [productId]: {
+        ...prevDetails[productId],
+        [name]: value,
+      },
+    }));
+  };
+
+  const startEditing = (productId, product) => {
+    setEditingProductId(productId);
+    setEditProductDetails((prevDetails) => ({
+      ...prevDetails,
+      [productId]: product,
+    }));
+  };
+  const filteredProducts = allProducts.filter(
+    (product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.category.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="listproduct">
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="search products..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)} 
+          className="search-input"
+        />
+      </div>
+
+      <div className="listproduct-format">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Category</p>
+        <p>Update</p>
+        <p>Remove</p>
+      </div>
+
+      <div className="listproduct-allproducts">
+        <hr />
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product, index) => (
+            <React.Fragment key={index}>
+              <div className="listproduct-format-main">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="listproduct-img"
+                />
+                {editingProductId === product._id ? (
+                  <>
+                    <input
+                      type="text"
+                      name="name"
+                      value={editProductDetails[product._id]?.name || ''} 
+                      onChange={(e) => handleEditChange(e, product._id)}
+                    />
+                    <input
+                      type="text"
+                      name="pricePerKg"
+                      value={editProductDetails[product._id]?.pricePerKg || ''} 
+                      onChange={(e) => handleEditChange(e, product._id)}
+                    />
+                    <select
+                      name="category"
+                      value={
+                        editProductDetails[product._id]?.category ||
+                        'defaultCategory'
+                      }
+                      onChange={(e) => handleEditChange(e, product._id)}
+                    >
+                      <option value="vegetables">Vegetables</option>
+                      <option value="fruits">Fruits</option>
+                      <option value="dryfruits">Dryfruits</option>
+                    </select>
+                    <button
+                      onClick={() => updateProduct(product._id)}
+                      className="listproduct-btn"
+                    >
+                      Save
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <p>{product.name}</p>
+                    <p>${product.pricePerKg}</p>
+                    <p>{product.category}</p>
+                    <button
+                      onClick={() => startEditing(product._id, product)}
+                      className="listproduct-btn"
+                    >
+                      Edit
+                    </button>
+                  </>
+                )}
+                <button
+                  onClick={() => removeProduct(product._id)}
+                  className="listproduct-btn"
+                >
+                  Remove
+                </button>
+              </div>
+              <hr />
+            </React.Fragment>
+          ))
+        ) : (
+          <p>No products found</p> 
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Listproduct;
